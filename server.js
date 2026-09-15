4f0bbb28f9ff54062520e9b7a90c16f4c2a3bc0c
Lenny Doiron <ldoiron@umass.edu>
2026-09-15 13:38:09 +0000

    Class 9/15

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ import express from "express";
 
 const app = express();
 const PORT = 3000;
+const events = [{ title: "Career fair" }, { title: "Hackathon kickoff" }];
+
+app.get("/events", (req, res) => {
+  res.render("events", { events });
+});
 
 app.get("/", (req, res) => {
   res.send("Hello, web!");
 });
+
+app.get();
 
 app.get("");
 
